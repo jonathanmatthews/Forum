86b3278b2312b14f03f8d661edf3efa28fd9fbd9
Najihah Amran <najihah.amran@enable.com>
2020-07-14 11:43:15 +0100

    Add initial home page layout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SidePanelComponent } from './features/side-panel/side-panel.component';
+import { ForumsListingComponent } from './features/forums-listing/forums-listing.component';
+import { SearchBarComponent } from './nav-bar/search-bar/search-bar.component';
+import { UserMenuComponent } from './nav-bar/user-menu/user-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    SidePanelComponent,
+    ForumsListingComponent,
+    SearchBarComponent,
+    UserMenuComponent
   ],
   imports: [
     BrowserModule,
