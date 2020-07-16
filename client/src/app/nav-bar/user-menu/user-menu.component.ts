1b50151131bc57beaef5cd8355a13f4840d9ed51
Najihah Amran <najihah.amran@enable.com>
2020-07-16 09:47:30 +0100

    Fix duplicate login and signup requests

--- client/src/app/nav-bar/user-menu/user-menu.component.ts
+++ client/src/app/nav-bar/user-menu/user-menu.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthClient, UserDto } from 'src/app/generated/forum-api.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Subject, Observable, of } from 'rxjs';
-import { takeUntil, catchError, tap } from 'rxjs/operators';
+import { Subject, Observable, of} from 'rxjs';
+import { takeUntil, catchError, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-menu',
@@ -13,7 +13,7 @@ export class UserMenuComponent implements OnInit, OnDestroy {
 
   constructor(private _authClient: AuthClient) { }
 
-  public user$: Observable<UserDto>;
+  public user: UserDto;
   public formTitle = 'Log in';
   public errorMessage = '';
   public loginForm = new FormGroup({
@@ -39,7 +39,7 @@ export class UserMenuComponent implements OnInit, OnDestroy {
   }
 
   public login(): void {
-    this.user$ = this._authClient.login([this.usernameInput, this.passwordInput])
+    this._authClient.login([this.usernameInput, this.passwordInput])
       .pipe(
         tap((user: UserDto) => {
           if (user) {
@@ -50,11 +50,15 @@ export class UserMenuComponent implements OnInit, OnDestroy {
           this.errorMessage = 'The username or password is incorrect. Please try again.';
           return of();
         }),
-        takeUntil(this._destroy$));
-  }
+        takeUntil(this._destroy$))
+          .subscribe(user => {
+            this.user = user;
+            }
+        );
+    }
 
   public createAccount(): void {
-    this.user$ = this._authClient.create([this.usernameInput, this.passwordInput])
+    this._authClient.create([this.usernameInput, this.passwordInput])
       .pipe(
         tap((user: UserDto) => {
           if (user) {
@@ -65,7 +69,10 @@ export class UserMenuComponent implements OnInit, OnDestroy {
           this.errorMessage = 'An error has occured. Please try again.';
           return of();
         }),
-        takeUntil(this._destroy$));
+        takeUntil(this._destroy$))
+          .subscribe(user => {
+            this.user = user;
+        });
   }
 
   public logout(): void {
