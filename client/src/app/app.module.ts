c23203258dc92ce2008a498f3259c53b71eeb00b
Najihah Amran <najihah.amran@enable.com>
2020-07-16 10:06:45 +0100

    Remove duplicate modules in app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,10 +14,7 @@ import { ForumsPanelComponent } from './features/forums-panel/forums-panel.compo
 import { ForumsHeaderComponent } from './features/forums-panel/forums-header/forums-header.component';
 import { ForumDetailsComponent } from './features/forum-details/forum-details.component';
 import { CommentsComponent } from './features/forum-details/comments/comments.component';
-import { AuthClient } from './generated/forum-api.service';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CreateForumComponent } from './features/create-forum/create-forum.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddCommentComponent } from './features/forum-details/comments/add-comment/add-comment.component';
 import { CommentsListingComponent } from './features/forum-details/comments/comments-listing/comments-listing.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
