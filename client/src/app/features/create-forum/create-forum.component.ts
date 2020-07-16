83e80e7b4aabcee63ee9a0493e37a25b1a4f4144
Najihah Amran <najihah.amran@enable.com>
2020-07-16 11:36:20 +0100

    Add api to create forum page

--- client/src/app/features/create-forum/create-forum.component.ts
+++ client/src/app/features/create-forum/create-forum.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { CategoryClient, CategoryDto, ForumDto } from 'src/app/generated/forum-api.service';
+import { Observable } from 'rxjs';
+import { Forum } from '../forum-details/forum-details.component';
 
 @Component({
   selector: 'app-create-forum',
@@ -8,22 +11,43 @@ import { FormGroup, Validators, FormControl } from '@angular/forms';
 })
 export class CreateForumComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _categoryService: CategoryClient) { }
+
+  public categories$: Observable<CategoryDto[]>;
 
   public formGroup = new FormGroup({
     title: new FormControl('', [Validators.minLength(2),
       Validators.maxLength(100), Validators.required]),
-    category: new FormControl('General'),
+    category: new FormControl(''),
     text: new FormControl('', [Validators.minLength(2),
       Validators.maxLength(4000)])
   });
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.categories$ = this._categoryService.listCategories(10, 0);
   }
 
   public createForum(): void {
-    const values = this.formGroup.value;
-    console.log(values);
+    if (!this.formGroup.valid) {
+      return;
+    }
+
+    const title = this.formGroup.controls.title.value as string;
+    const category = this.formGroup.controls.category.value as CategoryDto;
+    const text = this.formGroup.controls.text.value as string;
+
+    console.log(title);
+    console.log(category.id);
+    console.log(text);
+
+    const newForum = {
+      title: title,
+      text: text
+    } as ForumDto;
+
+    console.log(newForum);
+
+    //this._categoryService.createForum(category.id, newForum);
   }
 
 }
