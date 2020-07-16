558fd7da2cb92954d4669360d942050206bb00b3
Jonathan Matthews <jonathan.matthews@enable.com>
2020-07-16 12:08:40 +0100

    Connect comment submit page to api

--- client/src/app/features/forum-details/comments/add-comment/add-comment.component.ts
+++ client/src/app/features/forum-details/comments/add-comment/add-comment.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ForumClient, CommentDto } from 'src/app/generated/forum-api.service';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-comment',
@@ -8,19 +11,33 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class AddCommentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute, private _forumClient: ForumClient) { }
 
   public comment = new FormControl('', [Validators.minLength(1),
-    Validators.maxLength(4000), Validators.required]);
+  Validators.maxLength(4000), Validators.required]);
+
+  public unauthorised = false;
 
   ngOnInit(): void {
   }
 
   public createComment(): void {
     const text = this.comment.value;
-    console.log(text);
-
-    // TODO: send request to api
+    this._route.params.subscribe(params => {
+      const forumId = +params.id;
+      this._forumClient.createComment(forumId, { text } as CommentDto)
+        .pipe(
+          catchError(error => {
+            if (+error.status === 401) {
+              this.unauthorised = true;
+            }
+            return null;
+          })
+        )
+        .subscribe(val => {
+          this.unauthorised = false;
+          this.comment.reset();
+        });
+    });
   }
-
 }
