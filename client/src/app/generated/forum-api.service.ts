73f27f98e8418a4817e7f82fa4273e9242008e1e
Najihah Amran <najihah.amran@enable.com>
2020-07-21 11:42:40 +0100

    Update nswag

--- client/src/app/generated/forum-api.service.ts
+++ client/src/app/generated/forum-api.service.ts
@@ -718,6 +718,54 @@ export class ForumClient {
         return _observableOf<ForumDto[] | null>(<any>null);
     }
 
+    countAll(): Observable<number> {
+        let url_ = this.baseUrl + "/Forum/CountAll";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            withCredentials: true,
+            headers: new HttpHeaders({
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.request("get", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processCountAll(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processCountAll(<any>response_);
+                } catch (e) {
+                    return <Observable<number>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<number>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processCountAll(response: HttpResponseBase): Observable<number> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : <number>JSON.parse(_responseText, this.jsonParseReviver);
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<number>(<any>null);
+    }
+
     listComments(forumId: number, itemsPerPage: number | null | undefined, pageNumber: number | null | undefined): Observable<CommentDto[] | null> {
         let url_ = this.baseUrl + "/Forum/{forumId}/ListComments?";
         if (forumId === undefined || forumId === null)
